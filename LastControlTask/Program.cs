6e858f4e6104a09fc79dd45ae8c634e83271812c
Adelina Kharisova <h.adelina772gmail.com>
2023-04-28 16:48:39 +0300

    Добавлено решение задачи

--- LastControlTask/Program.cs
+++ LastControlTask/Program.cs
@@ -1,1 +1,20 @@
-﻿//
+﻿Console.WriteLine("Выберите количество строк в массиве: ");
+int count = int.Parse(Console.ReadLine());
+string[] str = new string[count];
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Введите элемент {i + 1}: ");
+    string A = Console.ReadLine();
+    str[i] = A;
+}
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3)
+    {
+        string[] minStr = new string[str.Length];
+        minStr[i] = str[i];
+        Console.WriteLine(minStr[i]);
+    }
+}
+Console.ReadLine();
